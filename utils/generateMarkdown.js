1333c3554e5979dfd916688b24034436813a7d5b
Erik Schaal <schaa077@umn.edu>
2020-07-02 16:46:46 -0500

    improved generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,30 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${data.desc}
+
+## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+
+## Installation
+  * ${data.install}
+## Usage
+  * ${data.usage}
+## License
+  * ${data.licence}
+## Contributing
+  * ${data.contribute}
+## Tests
+  * ${data.tests}
+## Questions
+  * For questions email me at ${data.email} or find me on github at https://github.com/${data.username}
 `;
 }
 
