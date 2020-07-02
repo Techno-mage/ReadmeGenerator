b922dcf262acd66725f859e987ea4c71f112aba4
Erik Schaal <schaa077@umn.edu>
2020-07-01 19:47:36 -0500

    inquirer questions are working

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //Imports
 const fx = require("fs");
 const util = require("util");
-const inquirer = require("infquirer");
+const inquirer = require("inquirer");
 
 
 // array of questions for user
 const questions = [
+    {type: "input", message: "What is the title of your project?", name: "title"},
+    {type: "input", message: "Write a brief description of your project", name: "desc"},
+    {type: "input", message: "Write down installation instructions if any.", name: "install"},
+    {type: "input", message: "Write a description of how to use this app.", name: "usage"},
+    {type: "input", message: "Please list the projects contributers.", name: "contribute"},
+    {type: "input", message: "Please include any testing if applicable.", name: "tests"},
+    {type: "list", message: "Please select a licence", choices: ["this", "that", "the other"], name:"licence"},
+    {type: "input", message: "Enter your Github user Name.", name: "username"},
+    {type: "input", message: "Enter your Email address.", name: "email"}
 
 ];
 
